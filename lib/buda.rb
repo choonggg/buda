bcb3392b4545cf011950b8d28456c8109d4079f8
Chong Hui <iamchoonggg@gmail.com>
2019-01-15 16:57:59 +0800

    initialize repo

---
+++ lib/buda.rb
@@ -0,0 +1,6 @@
+require "buda/version"
+
+module Buda
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/buda_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Buda do
+  it "has a version number" do
+    expect(Buda::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
